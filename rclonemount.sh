4a186d5e4c7225d0046b022761fd09394a29de6d
Ryan Erskine <ryan.j.erskine@gmail.com>
2019-04-07 23:55:23 -0500

    Updating to match specific settings.

--- rclonemount.sh
+++ rclonemount.sh
@@ -1,8 +1,1 @@
-rclone mount gcrypt: /GD \
-   --allow-other \
-   --read-only \
-   --dir-cache-time 48h \
-   --vfs-read-chunk-size 16M \
-   --vfs-read-chunk-size-limit 2G \
-   --buffer-size 512M \
-   --log-level INFO
+rclone mount googleDrive: ~/mnt/gdrive --allow-other --read-only --dir-cache-time 48h --vfs-read-chunk-size 16M --vfs-read-chunk-size-limit 2G --buffer-size 512M --log-level INFO &
